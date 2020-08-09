bfb3ae70efd894ffac2dcb52678dcef1022f4983
CemFFM <49092550+CemFFM@users.noreply.github.com>
2020-08-09 15:43:35 +0200

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import json
+
+from twitter_bot import TwitterBot
+
+
+def load_conf(filename):
+    with open(filename) as f:
+        return json.load(f)
+
+def main():
+    conf_file = '/Users/Cem/Desktop/twitter_bot-master/data/bots.conf'
+    conf = load_conf(conf_file)
+    for bot_username in conf['bot_usernames']:
+        try:
+            bot = TwitterBot(bot_username)
+
+            tweet = bot.tweet_random_quote()
+            if tweet:
+                msg = '%s tweeted: %s' % (bot_username, tweet.text)
+                print(msg)
+            else:
+                msg = '%s failed to tweet' % bot_username
+                print(msg)
+
+            result = bot.retweet_keyword_home_timeline()
+
+        except Exception as e:
+            error_msg = "%s ERROR: %s" % (bot_username, str(e))
+            print(error_msg)
+
+if __name__ == "__main__":
+    main()
---
+++ src/twitter_bot.py
@@ -0,0 +1,42 @@
+import tweepy
+import json
+from random import choice
+
+class TwitterBot:
+
+    def __init__(self, name):
+        botfile = open('../data/' + name, 'rb')
+        self.data = json.load(botfile)
+        botfile.close()
+        quote_file = open('../data/quotes.json','rb')
+        self.quotes_data = json.load(quote_file)
+        quote_file.close()
+        self.quotes = self.quotes_data['quotes']
+        auth = tweepy.OAuthHandler(self.data['consumer_key'], self.data['consumer_secret'])
+        auth.set_access_token(self.data['access_token'], self.data['access_token_secret'])
+        self.api = tweepy.API(auth)
+        self.retweet_keywords = self.data['retweet_keywords']
+
+    def tweet_random_quote(self):
+        trimmed_quote = self.random_quote()[:140]
+        quote = trimmed_quote.encode('ascii', 'replace')
+        print('quote to post: ', quote)
+        result = self.api.update_status(quote)
+        return result
+
+    def random_quote(self):
+        return choice(self.quotes)
+
+    def retweet_keyword_home_timeline(self):
+        statuses = self.api.home_timeline()
+        for s in statuses:
+            for keyword in self.retweet_keywords:
+                if keyword in s.text.lower():
+                    print('Keyword found: ',keyword)
+                    print('Status:',s.text)
+                    print('id=',s.id)
+                    resp = self.api.retweet(int(s.id))
+                    print('Resp retweet=',resp.text)
+
+
+        return None
